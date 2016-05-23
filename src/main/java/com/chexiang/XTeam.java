43ef084f0917868cf1105ce4d2f86a18fea919c0
apple <153614131@qq.com>
2016-05-24 00:02:33 +0800

    增加了模式匹配功能

--- src/main/java/com/chexiang/XTeam.java
+++ src/main/java/com/chexiang/XTeam.java
@@ -18,11 +18,10 @@ import static robocode.util.Utils.normalAbsoluteAngleDegrees;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 
-public class WALLE extends AdvancedRobot{
+public class XTeam extends AdvancedRobot{
     private static double BULLET_POWER = 3;
     private final double CIRCLE = 360;
     private double enemyAbsBearing = 0; //敌方绝对bearing = 我方朝向 + 相对bearing
-    private double myAbsBearingForEnemy;
     private boolean scaned = false;
     private double radarSpeed = CIRCLE/8;
     private Enemy preEnemy, curEnemy;
@@ -31,21 +30,8 @@ public class WALLE extends AdvancedRobot{
     private String diffHistory = "";
     private HashMap<String, int[]> historyMap = new HashMap<String, int[]>();
 
-    public void setEnemyAbsBearing(double enemyAbsBearing) {
-        this.enemyAbsBearing = enemyAbsBearing;
-        this.myAbsBearingForEnemy = reverseAngle(enemyAbsBearing);
-    }
-
-    public double getEnemyAbsBearing() {
-        return enemyAbsBearing;
-    }
-
-    public void setRadarSpeed(double radarSpeed) {
-        this.radarSpeed = radarSpeed;
-    }
-
     private void reverseRadar(){
-        setRadarSpeed(-radarSpeed);
+        radarSpeed = -radarSpeed;
     }
 
     @Override
@@ -57,17 +43,13 @@ public class WALLE extends AdvancedRobot{
         setTurnRadarRight(400);
         move();
         while(true) {
-            out.println("radarSpeed" + radarSpeed);
             setTurnRadarRight(radarSpeed);
             double radarHeading = getRadarHeading();
-//            if(scaned && !outOfEnemyRange(radarHeading) && outOfEnemyRange(radarHeading + radarSpeed)) {
-//                reverseRadar();
-//                scaned = false;
-//            }
+            if(scaned && !outOfEnemyRange(radarHeading) && outOfEnemyRange(radarHeading + radarSpeed)) {
+                reverseRadar();
+                scaned = false;
+            }
 
-//            ahead(100);
-//            turnGunLeft(70);
-//            back(100);
             if(getTurnRemaining() == 0) {
                 reverseHeading();
                 setTurnRight(CIRCLE / 3);
@@ -79,7 +61,7 @@ public class WALLE extends AdvancedRobot{
 
     private boolean outOfEnemyRange(double radarHeading) {
         radarHeading = Mod(radarHeading);
-        return Math.abs( minimizeTurn(radarHeading - getEnemyAbsBearing()) ) > CIRCLE/4;
+        return Math.abs( minimizeTurn(radarHeading - enemyAbsBearing) ) > CIRCLE/4;
     }
 
     @Override
@@ -87,21 +69,6 @@ public class WALLE extends AdvancedRobot{
 //        reverseHeading();
 //        move();
     }
-
-    private void turnAsReflect(double inAngle, double mirrorAngle) {
-        turnToTargetAngle( doReflect(inAngle, mirrorAngle) );
-        setAhead(50);
-        execute();
-    }
-
-    private double doReflect(double inAngle, double mirrorAngle) {
-        if(Math.abs( minimizeTurn(inAngle - mirrorAngle) ) > CIRCLE/4)
-            mirrorAngle = reverseAngle(mirrorAngle);
-//        out.println(String.format("doReflect\n inAngle: %f, mirrorAngle: %f\n" +
-//                "outAngle: %f\n", inAngle, mirrorAngle, Mod(2*mirrorAngle - inAngle)));
-        return Mod(2*mirrorAngle - inAngle);
-    }
-
 
     @Override
     public void onHitRobot(HitRobotEvent event) {
@@ -116,32 +83,21 @@ public class WALLE extends AdvancedRobot{
     @Override
     public void onHitByBullet(HitByBulletEvent event) {
         scan();
-        dodgeTheBullet();
+//        dodgeTheBullet();
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
         out.println("onScannedRobot: heading = " + event.getHeading());
         curEnemy = new Enemy(event);
+
         scaned = true;
-        setEnemyAbsBearing(Mod(this.getHeading() + event.getBearing()));
-//        reverseRadar();
+        enemyAbsBearing = Mod(this.getHeading() + event.getBearing());
 
         track();
 
-
-//        if(enemyMayFire(event))
-//            dodgeTheBullet();
-//        else {
-//            keepCrossWise();   //与敌方炮口保持横向(垂直)，以便躲避子弹
-            //setTurnGunRight(angle);
-//        }
-
-        //        aimAndFire();
-        if(curEnemy.diff != (char)-1) {
-            predictAndFire(event);
-//            track();
-        }
+        if(curEnemy.diff != (char)-1)
+            predictAndFire();
 
         preEnemy = new Enemy(event);
     }
@@ -152,7 +108,7 @@ public class WALLE extends AdvancedRobot{
 //        setTurnRadarRight(angleForRadarTurn);
     }
 
-    private void predictAndFire(ScannedRobotEvent event) {
+    private void predictAndFire() {
         record(diffHistory, curEnemy.diff);
         double angleForGunTurn = predictAngle(diffHistory);
         setTurnGunRight(angleForGunTurn);
@@ -164,7 +120,7 @@ public class WALLE extends AdvancedRobot{
         changePower();
 
         Point2D.Double myPos = new Point2D.Double(getX(), getY());
-        Point2D.Double enemyPos = getEnemyPos(myPos, Math.toRadians(getEnemyAbsBearing()), curEnemy.getDistance());
+        Point2D.Double enemyPos = getEnemyPos(myPos, Math.toRadians(enemyAbsBearing), curEnemy.distance);
         String pattern = new String(diffHistory);
         for (double d = 0; d < myPos.distance(enemyPos); d += Rules.getBulletSpeed(BULLET_POWER)) {
             char nextStep = predictDiff(pattern);
@@ -178,7 +134,7 @@ public class WALLE extends AdvancedRobot{
     }
 
     public void changePower() {
-        BULLET_POWER = Math.max(Math.min((getEnergy()-10)/10, 1000d / curEnemy.distance), 0.1);
+        BULLET_POWER = Math.max(Math.min((getEnergy())/3, 1000d / curEnemy.distance), 0.1);
     }
 
     private static Point2D.Double getEnemyPos(Point2D.Double p, double angle, double distance) {
@@ -212,74 +168,6 @@ public class WALLE extends AdvancedRobot{
         oldHistory += newDiff;
     }
 
-    private void aimAndFire() {
-        double angleForGunTurn = getAngleForGunTurn();
-        setTurnGunRight(angleForGunTurn);
-        if(Math.abs(angleForGunTurn) < 10) {
-            waitFor(new GunTurnCompleteCondition(this));
-            setFire(BULLET_POWER);
-        }
-    }
-
-    private double getAngleForGunTurn() {
-        //out.println("getAngleForGunTurn: adjustAngle: " + getAdjustAngle());
-        return minimizeTurn((getEnemyAbsBearing() - this.getGunHeading()) + getAdjustAngle());
-    }
-
-    private double getAdjustAngle() {   //正弦定理
-        double  bulletAngle = Mod(myAbsBearingForEnemy - curEnemy.getHeading()),    //sin(bulletAngle) 自带正负
-                bulletSpeed = Rules.getBulletSpeed(BULLET_POWER),
-                enemySpeed = curEnemy.getVelocity();
-        double enemyAngle = asin(Math.abs(sin(bulletAngle)) * (enemySpeed / bulletSpeed));
-
-        out.println(String.format("getAdjustAngle: myAbsBearingForEnemy = %f, curEnemy.getHeading = %f\n",
-                myAbsBearingForEnemy, curEnemy.getHeading()));
-
-        out.println(String.format("getAdjustAngle: bulletAngle = %f, bulletSpeed = %f, enemySpeed = %f\n" +
-                "enemyAngle = %f", bulletAngle, bulletSpeed, enemySpeed, enemyAngle));
-
-        //敌人前进方向转向对准你，所花的角度 bulletAngle <180,则在你右边
-        return bulletAngle < 180? enemyAngle: -enemyAngle;
-        //return enemyAngle;
-    }
-
-    private double asin(double x) {
-        return Math.toDegrees(Math.asin(x));
-    }
-
-    private double sin(double angle) {
-        return Math.sin(Math.toRadians(angle));
-    }
-
-    private void keepCrossWise() {
-        out.println(String.format("getBearing: %f\n", getEnemyAbsBearing()));
-        turnToTargetAngle( Mod(getEnemyAbsBearing() + CIRCLE/4) );
-    }
-
-    /**
-     * 车轮转到指定角度
-     * @param targetAngle 指定角度 [0,360)
-     */
-    private void turnToTargetAngle(double targetAngle) {
-        out.println(String.format("targetAngle: %f, Heading: %f\n", targetAngle, getHeading()));
-        setTurnRight(minimizeTurn(targetAngle - getHeading()));
-    }
-
-    private void dodgeTheBullet() {
-        //setAhead(50);
-        move();
-    }
-
-    private boolean enemyMayFire(ScannedRobotEvent event) {
-        out.println("on enemyMayFire");
-        return costEnergyForFire(preEnemy.getEnergy() - event.getEnergy());
-    }
-
-    private boolean costEnergyForFire(double changedEnergy) {
-        out.println("on costEnergyForFire" + changedEnergy);
-        return changedEnergy>=0.1 && changedEnergy<=3;
-    }
-
     private double Mod(double angle){   //取模,规整为[0~360)
         return normalAbsoluteAngleDegrees(angle);
 //        double tmp = angle + CIRCLE * 999999;
@@ -292,17 +180,12 @@ public class WALLE extends AdvancedRobot{
 //        return angle>CIRCLE/2? angle-CIRCLE: angle;
     }
 
-    private double reverseAngle(double angle) {
-        return Mod(angle + CIRCLE/2);
-    }
-
     private void move(){
         if(isMovingAhead)
             setAhead(5000000);
         else
             setBack(5000000);
     }
-
 
     private class Enemy{
         public String name;
@@ -346,61 +229,6 @@ public class WALLE extends AdvancedRobot{
                     event.getDistance(),
                     event.getVelocity()
             );
-
         }
-
-//        public Enemy() {
-//            this("", 0, 0, 0, 0, 0);
-//        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public double getEnergy() {
-            return energy;
-        }
-
-        public void setEnergy(double energy) {
-            this.energy = energy;
-        }
-
-        public double getHeading() {
-            return heading;
-        }
-
-        public void setHeading(double heading) {
-            this.heading = heading;
-        }
-
-        public double getBearing() {
-            return bearing;
-        }
-
-        public void setBearing(double bearing) {
-            this.bearing = bearing;
-        }
-
-        public double getDistance() {
-            return distance;
-        }
-
-        public void setDistance(double distance) {
-            this.distance = distance;
-        }
-
-        public double getVelocity() {
-            return velocity;
-        }
-
-        public void setVelocity(double velocity) {
-            this.velocity = velocity;
-        }
-
     }
-
 }
